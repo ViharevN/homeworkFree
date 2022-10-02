d99d352058b7c7b4ebe890866b603272d50340ae
ViharevN <swix_off@mail.ru>
2022-10-02 23:03:15 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //задание 1
+
+        int oneInt = 10;
+        System.out.println(oneInt);
+        double oneDouble = 20.33;
+        System.out.println(oneDouble);
+        float oneFloat = 20.34f;
+        System.out.println(oneFloat);
+        char symbol = 'C';
+        System.out.println(symbol);
+        short oneShort = 100;
+        System.out.println(oneShort);
+        long oneLong = 10000l;
+        System.out.println(oneLong);
+        byte oneByte = 99;
+        System.out.println(oneByte);
+
+
     }
 }
